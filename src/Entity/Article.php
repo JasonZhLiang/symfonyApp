df8f8089d422e9367fad16f7a766bde52c1471e4
JasonZhLiang <Jasonzh.liang@gmail.com>
2019-02-04 15:36:44 -0500

    Timestampable and sluggable of Doctrine extensions, and also Fixtures and Faker for dummy data

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,12 +3,16 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
  */
 class Article
 {
+    //instead of creating createAt and updateAt manually,
+    use TimestampableEntity;
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -23,6 +27,7 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=100, unique=true)
+     * @Gedmo\Slug(fields={"title"})
      */
     private $slug;
 
@@ -50,6 +55,18 @@ class Article
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $imageFilename;
+
+//    /**
+//     * @ORM\Column(type="datetime")
+//     * @Gedmo\Timestampable(on="create")
+//     */
+//    private $createdAt;
+//
+//    /**
+//     * @ORM\Column(type="datetime")
+//     * @Gedmo\Timestampable(on="update")
+//     */
+//    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -150,4 +167,28 @@ class Article
         $this->heartCount = $this->heartCount +1;
         return $this;
     }
+
+//    public function getCreatedAt(): ?\DateTimeInterface
+//    {
+//        return $this->createdAt;
+//    }
+//
+//    public function setCreatedAt(\DateTimeInterface $createdAt): self
+//    {
+//        $this->createdAt = $createdAt;
+//
+//        return $this;
+//    }
+//
+//    public function getUpdatedAt(): ?\DateTimeInterface
+//    {
+//        return $this->updatedAt;
+//    }
+//
+//    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+//    {
+//        $this->updatedAt = $updatedAt;
+//
+//        return $this;
+//    }
 }
