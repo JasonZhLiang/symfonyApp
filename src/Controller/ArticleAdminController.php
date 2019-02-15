cd94314b1b73732e9fc02df4714e1f05985e34df
JasonZhLiang <Jasonzh.liang@gmail.com>
2019-02-15 16:04:10 -0500

    create article new form and edit form, installed validator for these form to sanity and business rule validation

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -6,10 +6,12 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleFormType;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use function GuzzleHttp\Psr7\_parse_request_uri;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,7 +22,7 @@ class ArticleAdminController extends AbstractController
      * @Route("admin/article/new", name="admin_article_new")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em, Request $request)
     {
         //*************use fixtures, so will delete this block below************
 //        $article = new Article();
@@ -60,9 +62,6 @@ class ArticleAdminController extends AbstractController
 //        $em->flush();
         //*****************************move above block to fixtures class************************
 
-
-
-
 //        return new Response(sprintf(
 //            'Hiya! New article id: #%d slug: :%s',
 //            $article->getId(),
@@ -70,22 +69,69 @@ class ArticleAdminController extends AbstractController
 //        ));
 
         $form = $this->createForm(ArticleFormType::class);
+
+        $form->handleRequest($request);//only handle the data when it's a post request
+        if ($form->isSubmitted() && $form->isValid()){
+//            $data = $form->getData();
+//            dd($data);
+//            $article = new Article();
+//            $article->setTitle($data['title']);
+//            $article->setContent($data['content']);
+
+            /** @var Article $article */
+            $article = $form->getData();//since in ArticleFormType class we bind the form to Article, so now $form->getData return Article object directly.
+//            $article->setAuthor($this->getUser());//since getUser is not a field of form getData now, so we need keep setAuthor here, we will remove it once set this field
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success','Article Created! Knowledge is power!');//is a shortcut to set a message in the session. But, flash messages only live in the session until they are read for the first time.
+
+            return $this->redirectToRoute('admin_article_list');
+        }
         return $this->render('article_admin/new.html.twig',[
             'articleForm' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/admin/article/{id}/edit")
+     * @Route("/admin/article/{id}/edit", name="admin_article_edit")
      * @IsGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article)
+    public function edit(Article $article, Request $request, EntityManagerInterface $em)
     {
 //        if ($article->getAuthor() != $this->getUser() && !$this->isGranted('ROLE_ADMIN_ARTICLE')){//to centralize this logic for reusing, use voter
 //        if (!$this->isGranted('MANAGE', $article)){
 //            throw $this->createAccessDeniedException('No access!');
 //        }
 //        $this->denyAccessUnlessGranted('MANAGE',$article);//this is s shortcut for above, can move to annotation
-        dd($article);
+//        dd($article);
+
+        $form = $this->createForm(ArticleFormType::class, $article);
+
+        $form->handleRequest($request);//only handle the data when it's a post request
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $em->persist($article);
+            $em->flush();
+            $this->addFlash('success','Article Updated! Inaccuracies Squashed!');//is a shortcut to set a message in the session. But, flash messages only live in the session until they are read for the first time.
+            return $this->redirectToRoute('admin_article_edit',[
+                'id' => $article->getId(),
+            ]);
+        }
+        return $this->render('article_admin/edit.html.twig',[
+            'articleForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/article", name="admin_article_list")
+     */
+    public function list(ArticleRepository $articleRepo)
+    {
+        $articles = $articleRepo->findAll();
+        return $this->render('article_admin/list.html.twig',[
+            'articles' => $articles,
+        ]);
     }
 }
